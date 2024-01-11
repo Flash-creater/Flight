b3325482f03cca386801f4a0973c32b641b36ab9
bill <firegoldfish@outlook.com>
2024-01-11 22:58:41 +0800

    agencyMapper写了几个

--- flight/src/main/java/com/flight/dao/Impl/AgencyDaoImpl.java
+++ flight/src/main/java/com/flight/dao/Impl/AgencyDaoImpl.java
@@ -116,12 +116,12 @@ public class AgencyDaoImpl implements AgencyDao {
     }
     //根据查询到的tid查找对应的traveller的信息
     @Override
-    public Traveller findTraById(Integer tid) throws Exception {
-        String sql = "select * from traveller where id = ?";
-        Traveller traveller = null;
-        traveller = template.queryForObject(sql, new BeanPropertyRowMapper<Traveller>(Traveller.class), tid);
-        return traveller;
-    }
+//    public Traveller findTraById(Integer tid) throws Exception {
+//        String sql = "select * from traveller where id = ?";
+//        Traveller traveller = null;
+//        traveller = template.queryForObject(sql, new BeanPropertyRowMapper<Traveller>(Traveller.class), tid);
+//        return traveller;
+//    }
     //根据aid和tid查询出对应的所有的路线id
     @Override
     public List<Route> findRidsByTidAndAid(Integer tid, String aid) throws Exception{
@@ -131,12 +131,12 @@ public class AgencyDaoImpl implements AgencyDao {
     }
     //根据tid,rid,aid查询出该订单对应的下单时间，下单状态，以及处理订单的时间
     @Override
-    public AgencyOrder findAgencyOrder(String aid, Integer tid, String rid) throws Exception{
-        String sql = "select * from agency_order where aid=? and tid =? and routeId=?";
-        AgencyOrder ao = null;
-        ao = template.queryForObject(sql, new BeanPropertyRowMapper<AgencyOrder>(AgencyOrder.class),aid,tid,rid);
-        return ao;
-    }
+//    public AgencyOrder findAgencyOrder(String aid, Integer tid, String rid) throws Exception{
+//        String sql = "select * from agency_order where aid=? and tid =? and routeId=?";
+//        AgencyOrder ao = null;
+//        ao = template.queryForObject(sql, new BeanPropertyRowMapper<AgencyOrder>(AgencyOrder.class),aid,tid,rid);
+//        return ao;
+//    }
     //根据tid和aid查询出对应的所有的路线id
 
     @Override
@@ -149,27 +149,27 @@ public class AgencyDaoImpl implements AgencyDao {
 
     //根据对应的routeId，aid, tid查找唯一的订单
     @Override
-    public AgencyOrder findOrderById(String aid, String routeId, Integer tid) {
-        String sql = "select * from agency_order where aid=? and routeId = ? and tid= ?";
-        AgencyOrder ao = null;
-        try {
-            ao = template.queryForObject(sql, new BeanPropertyRowMapper<AgencyOrder>(AgencyOrder.class), aid, routeId, tid);
-            return ao;
-        } catch (DataAccessException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+//    public AgencyOrder findOrderById(String aid, String routeId, Integer tid) {
+//        String sql = "select * from agency_order where aid=? and routeId = ? and tid= ?";
+//        AgencyOrder ao = null;
+//        try {
+//            ao = template.queryForObject(sql, new BeanPropertyRowMapper<AgencyOrder>(AgencyOrder.class), aid, routeId, tid);
+//            return ao;
+//        } catch (DataAccessException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
 
     //根据id值查找旅行社
     @Override
-    public Agency findAgencyById(String id) throws Exception{
-        String sql = "select * from agency where id = ?";
-        Agency agency = null;
-        agency = template.queryForObject(sql, new BeanPropertyRowMapper<Agency>(Agency.class), id);
-        return agency;
-
-    }
+//    public Agency findAgencyById(String id) throws Exception{
+//        String sql = "select * from agency where id = ?";
+//        Agency agency = null;
+//        agency = template.queryForObject(sql, new BeanPropertyRowMapper<Agency>(Agency.class), id);
+//        return agency;
+//
+//    }
     //通过agency的id值查找对应的路线
     @Override
     public List<Route> findRouteByAid(String id) {
