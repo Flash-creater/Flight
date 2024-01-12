a5cbd62d24634421148e5511c345b0d75013d6b7
bill <firegoldfish@outlook.com>
2024-01-12 15:09:37 +0800

    agencyMapper完成

--- src/main/java/com/tyut/mapper/FlightMapper.java
+++
@@ -1,7 +0,0 @@
-package com.tyut.mapper;
-import java.util.List;
-import com.tyut.domain.Flight;
-
-public interface FlightMapper {
-    public List <Flight> findByDepAndFin(String departureCity, String FinalCity);
-}
